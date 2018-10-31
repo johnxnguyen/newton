651a7afbde7682d07b9aae3f02edf90a1c02bcaa
John Nguyen <johnxuannguyen@gmail.com>
2018-10-31 16:14:35 +0100

    Restructure force calculations

--- src/body.rs
+++ src/body.rs
@@ -1,17 +1,35 @@
 use point::Point;
 use vector::Vector;
+use std::cmp::Eq;
 
+// TODO: Needs testing
+
+#[derive(Debug)]
 pub struct Body {
+    pub id: u32,
     pub mass: f64,
     pub position: Point,
     pub velocity: Vector,
+}
+
+impl Eq for Body {}
+
+impl PartialEq for Body {
+    /**
+     *  Bodies are compared referentially.
+     */
+    fn eq(&self, other: &'_ Body) -> bool {
+        self as *const _ == other as *const _
+    }
 }
 
 impl Body {
     /**
      *  Apply the given force to update the velocity vector.
      */
-    fn apply_force(&mut self, force: Vector) {
+    pub fn apply_force(&mut self, force: &Vector) {
         self.velocity += force / self.mass;
+        self.position.x += force.dx.round() as i32;
+        self.position.y += force.dy.round() as i32;
     }
 }
--- src/field.rs
+++ src/field.rs
@@ -1,5 +1,6 @@
 use body::Body;
 use vector::Vector;
+use std::collections::HashMap;
 
 pub struct Field {
     pub g: f64,
@@ -8,6 +9,54 @@ pub struct Field {
 
 impl Field {
 
+    // TODO: Needs testing
+    /**
+     *  Update the state of the field by applying force on each of the bodies
+     *  and updating their positions.
+     */
+    pub fn update(&mut self) {
+
+        let force_map = self.force_map();
+
+        // now the idea is to iterate through each body and update
+        // its velocity, then its position.
+
+        for body in self.bodies.iter_mut() {
+            match force_map.get(&body.id) {
+                Some(force) => body.apply_force(force),
+                None => ()
+            }
+        }
+
+    }
+
+    // TODO: Needs testing
+    /**
+     *  A mapping of Body ids to their corresponding received forces
+     *  for the current state. This uses a brute force approach with O(n^2).
+     */
+    fn force_map(&self) -> HashMap<u32, Vector> {
+        // store the forces for each body
+        let mut forces: HashMap<u32, Vector> = HashMap::new();
+
+        for body in self.bodies.iter() {
+
+            let mut cumulative_force = Vector::zero();
+
+            // combine the forces of all other bodies exerted on body
+            for other in self.bodies.iter() {
+                if body != other {
+                    cumulative_force += self.force_between(body, other);
+                }
+            }
+
+            forces.insert(body.id, cumulative_force);
+        }
+
+        forces
+    }
+
+    // TODO: Needs testing
     /**
      *  The force exerted mutually between the given bodies.
      */
