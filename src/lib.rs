fbfafbfef8f86390ec3acdfbdad8830d121d49ab
John Nguyen <johnxuannguyen@gmail.com>
2018-11-04 20:31:04 +0100

    Define public API

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,60 @@ pub mod point;
 pub mod vector;
 pub mod body;
 pub mod field;
+
+use field::Field;
+use body::Body;
+use point::Point;
+use vector::Vector;
+use std::i32;
+
+#[no_mangle]
+pub extern fn newton_new_field(g: f64) -> *mut Field {
+    let field = Field { g, bodies: vec![] };
+    let boxed = Box::new(field);
+    println!("A field has been allocated");
+    Box::into_raw(boxed)
+}
+
+#[no_mangle]
+pub unsafe extern fn newton_destroy_field(field: *mut Field) {
+    let _ = Box::from_raw(field);
+}
+
+#[no_mangle]
+pub unsafe extern fn newton_add_body(field: *mut Field, id: u32, mass: f64, x: i32, y: i32, dx: f64, dy: f64) {
+    let body = Body {
+        id,
+        mass,
+        position: Point { x, y },
+        velocity: Vector { dx, dy },
+    };
+
+    println!("Body #{} is created.", id);
+    let field = &mut *field;
+    field.bodies.push(body);
+}
+
+#[no_mangle]
+pub unsafe extern fn newton_step(field: *mut Field) {
+    let field = &mut *field;
+    field.update()
+}
+
+#[no_mangle]
+pub unsafe extern fn newton_body_x_pos(field: *const Field, id: u32) -> i32 {
+    let field = &* field;
+    match field.bodies.get(id as usize) {
+        Some(val) => (val as &Body).position.x,
+        None => i32::MAX
+    }
+}
+
+#[no_mangle]
+pub unsafe extern fn newton_body_y_pos(field: *const Field, id: u32) -> i32 {
+    let field = &* field;
+    match field.bodies.get(id as usize) {
+        Some(val) => (val as &Body).position.y,
+        None => i32::MAX
+    }
+}
---
+++ src/newton.h
@@ -0,0 +1,54 @@
+//
+// Created by John Nguyen on 04.11.18.
+//
+
+#ifndef NEWTON_NEWTON_H
+#define NEWTON_NEWTON_H
+
+#include <stdint.h>
+
+/**
+ *  A opaque struct representing the
+ *  gravitational field.
+ */
+struct field;
+
+/**
+ *  Allcoates a new field instance with
+ *  the given gravitational constant.
+ */
+struct field *newton_new_field(double g);
+
+/**
+ *  Destroys the field instance referred
+ *  to by the given pointer.
+ */
+void newton_destroy_field(struct field *field);
+
+/**
+ *  Creates a new body instance with the
+ *  given properties (id, mass, position, velocity)
+ *  and adds it to the field.
+ */
+void newton_add_body(struct field *field, uint32_t id, double mass, int32_t x, int32_t y, double dx, double dy);
+
+ /**
+  *  Advances the field state by a single step.
+  */
+ void newton_step(struct field *field);
+
+ /**
+  *  Returns the x coordinate of the body with the
+  *  given id, if it exists, else the max value of
+  *  int32_t.
+  */
+int32_t newton_body_x_pos(const struct field *field, uint32_t id);
+
+/**
+*  Returns the y coordinate of the body with the
+*  given id, if it exists, else the max value of
+*  int32_t.
+*/
+int32_t newton_body_y_pos(const struct field *field, uint32_t id);
+
+#endif //NEWTON_NEWTON_H
