1dc0d22225e40300d976f7530f61af427d0954c6
John Nguyen <johnxuannguyen@gmail.com>
2018-11-10 19:54:37 +0100

    Add transformation tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,4 @@
 mod vector_tests;
 mod point_tests;
 mod body_tests;
+mod transformation_tests;
--- src/transformation.rs
+++ src/transformation.rs
@@ -1,28 +1,28 @@
 use vector::Vector;
 use std::ops::Mul;
 
-struct Transformation {
-    a: Vector,
-    b: Vector,
+pub struct Transformation {
+    pub a: Vector,
+    pub b: Vector,
 }
 
-// TODO: Needs testing
+/**
+ *  Transformation of a vector.
+ */
 impl<'a> Mul<Vector> for &'a Transformation {
     type Output = Vector;
 
-    /**
-     *  Transformation of a vector.
-     */
     fn mul(self, rhs: Vector) -> Self::Output {
         &self.a * rhs.dx + &self.b * rhs.dy
     }
 }
 
 impl Transformation {
-
-    // TODO: Needs testing
-    fn rotation(theta: f64) -> Transformation {
-        let (sin, cos) = theta.sin_cos();
+    /**
+     *  Rotation transformation.
+     */
+    pub fn rotation(radians: f64) -> Transformation {
+        let (sin, cos) = radians.sin_cos();
         Transformation {
             a: Vector { dx: cos, dy: sin },
             b: Vector { dx: -sin, dy: cos },
