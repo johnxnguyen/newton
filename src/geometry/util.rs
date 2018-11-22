accb41c1f6c924f994684ddb4562e6b7ad290f30
John Nguyen <johnxuannguyen@gmail.com>
2018-11-22 22:40:46 +0100

    Change return type of Distributor to Vec<Body>

--- src/geometry/util.rs
+++ src/geometry/util.rs
@@ -3,7 +3,6 @@ use physics::types::Body;
 use rand::prelude::*;
 use std::f32::consts::PI;
 use std::ops::Mul;
-use std::collections::HashMap;
 
 // Transformation ////////////////////////////////////////////////////////////
 //
@@ -40,8 +39,8 @@ pub struct Distributor {
 }
 
 impl Distributor {
-    pub fn distribution(&self) -> HashMap<u32, Body> {
-        let mut result: HashMap<u32, Body> = HashMap::new();
+    pub fn distribution(&self) -> Vec<Body> {
+        let mut result: Vec<Body> = vec![];
         let mut angle_rand = thread_rng();
         let mut dist_rand = thread_rng();
 
@@ -66,7 +65,7 @@ impl Distributor {
             };
 
             let body = Body::new(i, 0.1, pos, velocity);
-            result.insert(i, body);
+            result.push(body);
         }
 
         result
