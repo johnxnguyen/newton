77e60f78269aae9c66149ec255336a770f10662b
John Nguyen <johnxuannguyen@gmail.com>
2018-11-24 20:23:57 +0100

    Add Gravity tests

--- src/physics/force.rs
+++ src/physics/force.rs
@@ -61,3 +61,92 @@ impl Attractor {
         self.gravity.between(body, &self.body)
     }
 }
+
+// Tests /////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::{Gravity, Body};
+    use geometry::types::{Point, Vector};
+
+    #[test]
+    fn gravity_calculates_force() {
+        // given
+        let sut = Gravity::new(1.5, 4.0);
+
+        let b1 = Body::new(
+            0,
+            1.0,
+            Point { x: 1.0, y: 2.0},
+            Vector::zero()
+        );
+
+        let b2 = Body::new(
+            1,
+            2.0,
+            Point { x: -3.5, y: 0.0},
+            Vector::zero()
+        );
+
+        // when
+        let result = sut.between(&b1, &b2);
+
+        // then
+        assert_eq!(result, Vector { dx: -0.1130488514, dy: -0.0502439339});
+    }
+
+    #[test]
+    fn gravity_obeys_minimum_distance() {
+        // given
+        let sut = Gravity::new(1.5, 4.0);
+
+        let b1 = Body::new(
+            0,
+            1.0,
+            Point { x: 1.0, y: 2.0},
+            Vector::zero()
+        );
+
+        let b2 = Body::new(
+            1,
+            2.0,
+            Point { x: 2.0, y: 2.5},
+            Vector::zero()
+        );
+
+        assert!(b1.position.distance_to(&b2.position) < 4.0);
+
+        // when
+        let result = sut.between(&b1, &b2);
+
+        // then
+        let result_if_dist_was_4 = Vector { dx: 0.1677050983, dy: 0.0838525491};
+        assert_eq!(result, result_if_dist_was_4);
+    }
+
+    #[test]
+    fn gravity_is_undefined_for_bodies_with_equal_position() {
+        // given
+        let sut = Gravity::new(1.5, 4.0);
+
+        let b1 = Body::new(
+            0,
+            1.0,
+            Point { x: 1.0, y: 2.0},
+            Vector::zero()
+        );
+
+        let b2 = Body::new(
+            1,
+            2.0,
+            Point { x: 1.0, y: 2.0},
+            Vector::zero()
+        );
+
+        // when
+        let result = sut.between(&b1, &b2);
+
+        // then
+        assert_eq!(result, Vector::zero());
+    }
+}
