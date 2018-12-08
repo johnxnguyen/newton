15d9183479495ac00c6e29e733a0a555929e8ccc
John Nguyen <johnxuannguyen@gmail.com>
2018-12-08 15:06:45 +0100

    Add descriptions

--- src/physics/barneshut.rs
+++ src/physics/barneshut.rs
@@ -7,15 +7,42 @@ use geometry::types::Rect;
 
 use super::types::Body;
 
-type Index = u32;
+// Action ////////////////////////////////////////////////////////////////////
+//
+// Represents an action to take on a `BHTree`. Either insert the given node
+// or internalize the node at the given index, before considering the
+// pending state.
 
-enum Changes {
+enum Action {
     Insert(Node),
     Internalize(Index, Pending),
 }
 
-/// Represents a pending state. Body needs to be added at Index
+// Pending ///////////////////////////////////////////////////////////////////
+//
+// Represents an intermediate state of the insertion algorithm. When adding
+// a body to the tree it is inspected top down. It could happen that a node
+// in the path needs to be internalized before the body can be inserted, in
+// which case, the current index and the body need to be stored temporarily
+// so that the insertion algorithm can continue at a later time.
+
 struct Pending(Index, Body);
+
+// BHTree ////////////////////////////////////////////////////////////////////
+//
+// The Barnes-Hut Tree is a quadtree used to efficiently calculate forces
+// between n bodies. It requires O(nlogn) time to calculate force vectors for
+// each of the n bodies, a substantial improvement over the quadratic brute
+// force algorithm.
+//
+// It achieves this by spatially storing the bodies in its leaves. The force
+// vector for each body is then calculated by traversing the tree across
+// internal nodes of varying depth. These internal nodes are viewed as virtual
+// bodies and are used to calculate the force vectors. The body's final force
+// vector is simply the sum of all intermediate force vectors at each virtual
+// body.
+
+type Index = u32;
 
 #[derive(Debug)]
 pub struct BHTree {
@@ -23,7 +50,7 @@ pub struct BHTree {
 }
 
 impl BHTree {
-    /// Initialized the tree with a root node.
+    /// Initialized tree with a root nodes spanning the given space.
     pub fn new(space: Rect) -> BHTree {
         let mut nodes: HashMap<Index, Node> = HashMap::new();
         let root = Node::new(0, space, None);
@@ -51,27 +78,28 @@ impl BHTree {
 
     /// Inserts the given body into the tree at the given node.
     fn insert(&mut self, pending: Pending) {
-        let changes: Changes;
+        let action: Action;
         {
-            // inspect the tree to find the necessary changes
-            changes = self.changes(self.node(&pending.0).unwrap(), pending.1);
+            // inspect the tree to find the necessary action
+            action = self.action(self.node(&pending.0).unwrap(), pending.1);
         }
         {
-            match self.process(changes) {
+            match self.process(action) {
                 Some(pending) => self.insert(pending),
                 None => (),
             }
         }
     }
 
-    /// Processes the change by either inserting, moving, or doing nothing.
-    fn process(&mut self, change: Changes) -> Option<Pending> {
-        match change {
-            Changes::Insert(node) => {
+    /// Processes the action by either inserting a new node, internalizing an
+    /// existing node, or doing nothing. Optionally returns a pending state.
+    fn process(&mut self, action: Action) -> Option<Pending> {
+        match action {
+            Action::Insert(node) => {
                 self.nodes.insert(node.id, node);
                 None
             },
-            Changes::Internalize(id, pending) => {
+            Action::Internalize(id, pending) => {
                 self.internalize(id);
                 Some(pending)
             },
@@ -98,22 +126,31 @@ impl BHTree {
     /// needs to be internalize. Additionally, the current index and body
     /// is included in the return so that the search can continue after
     /// the leaf has been internalized.
-    fn changes(&self, node: &Node, body: Body) -> Changes {
+    fn action(&self, node: &Node, body: Body) -> Action {
         debug_assert!(node.space.contains(&body.position));
 
         if self.is_leaf(node) {
-            if node.is_empty() { Changes::Insert(node.with(body)) }
-            else { Changes::Internalize(node.id, Pending(node.id, body)) }
+            if node.is_empty() { Action::Insert(node.with(body)) }
+            else { Action::Internalize(node.id, Pending(node.id, body)) }
         } else {
             node.map_quadrant(body.position.clone(), move |idx: Index, q: Quadrant| {
                 match self.node(&idx) {
-                    Some(child) => self.changes(child, body),
-                    None => Changes::Insert(Node::new(idx, q.space().clone(), Some(body))),
+                    Some(child) => self.action(child, body),
+                    None => Action::Insert(Node::new(idx, q.space().clone(), Some(body))),
                 }
             })
         }
     }
 }
+
+// Node //////////////////////////////////////////////////////////////////////
+//
+// Represents a node in the BHTree. Since the BHTree uses indices to relate
+// nodes together, each node must minimally be aware of its own index. The
+// indices of related nodes can be calculated in constant time and thus
+// do not need to be stored.
+
+// TODO: Consider if its possible to use an enum
 
 #[derive(Clone, Debug)]
 struct Node {
@@ -157,7 +194,7 @@ impl Node {
     /// Index of the south east child.
     fn se(&self) -> Index { 4 * self.id + 4 }
 
-    /// Moves the self body into a new child node and returns it, if it exists.
+    /// Moves the body into a new child node and returns it, if it exists.
     fn child_from_self(&mut self) -> Option<Node> {
         let body = match self.body.take() {
             None => return None,
@@ -174,8 +211,7 @@ impl Node {
     /// Finds quadrant containing the given point and passes it together with
     /// its index to the function f. Panics if the point is out of bounds.
     fn map_quadrant<U, F>(&self, point: Point, f: F) -> U
-        where F: FnOnce(Index, Quadrant) -> U {
-
+    where F: FnOnce(Index, Quadrant) -> U {
         let quadrant = self.space.quadrant(&point).unwrap_or_else(|err| {
             panic!("Couldn't find quadrant: {}", err.kind());
         });
@@ -188,6 +224,8 @@ impl Node {
         }
     }
 }
+
+// Tests /////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
