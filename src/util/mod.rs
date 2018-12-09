dafdeb39429c94bb78338901028758319cd28dff
vancouver <vancouver2990@gmail.com>
2018-12-09 14:11:06 +0100

    fix build error

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,6 @@
 use geometry::types::Point;
 use std::fs::File;
+use std::io::Write;
 
 pub fn write_points(points: Vec<Point>, file_name: &str) -> std::io::Result<()> {
     let mut file = File::create(file_name)?;
