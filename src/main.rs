144aeecfb0479d13cdfb2e867bcaa3c2a378284c
John Nguyen <johnxuannguyen@gmail.com>
2018-12-12 23:25:22 +0100

    Add main function for testing

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate newton;
+
+use newton::physics::types::Environment;
+use newton::geometry::util::Distributor;
+
+fn main() {
+    let mut env = Environment::new();
+    let distributor = Distributor {
+        num_bodies: 1000,
+        min_dist: 50.0,
+        max_dist: 250.0,
+        dy: 10.0
+    };
+
+    env.bodies = Some(distributor.distribution());
+
+    for _ in 0..150 {
+        env.step();
+    }
+}
