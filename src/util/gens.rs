6743f68b77ed5579f1354d4afd7d9dba922094fb
John Nguyen <johnxuannguyen@gmail.com>
2019-01-04 22:40:54 +0100

    Create PositiveUniformGen

--- src/util/gens.rs
+++ src/util/gens.rs
@@ -5,28 +5,46 @@ use rand::ThreadRng;
 
 use physics::types::Mass;
 
-// MassGen ///////////////////////////////////////////////////////////////////
+// PositiveUniformGen ////////////////////////////////////////////////////////
 //
-// Uniformly generates random Mass within a closed range of f32 values.
+// Uniformly generates random f32 within a closed range of positive values.
 
-struct MassGen {
+struct PositiveUniformGen {
     distribution: Uniform<f32>,
     rand: ThreadRng,
 }
 
-impl MassGen {
-    fn new(low: f32, high: f32) -> MassGen {
+impl PositiveUniformGen {
+    fn new(low: f32, high: f32) -> PositiveUniformGen {
         if low <= 0.0 || high <= 0.0 {
-            panic!("MassGen requires positive range. Got [{}, {}]", low, high);
+            panic!("PositiveUniformGen requires positive range. Got [{}, {}]", low, high);
         }
-        MassGen {
+        PositiveUniformGen {
             distribution: Uniform::new_inclusive(low, high),
             rand: thread_rng(),
         }
     }
 
+    fn next(&mut self) -> f32 {
+        self.rand.sample(self.distribution)
+    }
+}
+
+// MassGen ///////////////////////////////////////////////////////////////////
+//
+// Uniformly generates random Mass within a closed range.
+
+struct MassGen {
+    gen: PositiveUniformGen,
+}
+
+impl MassGen {
+    fn new(low: f32, high: f32) -> MassGen {
+        MassGen { gen: PositiveUniformGen::new(low, high) }
+    }
+
     fn next(&mut self) -> Mass {
-        Mass::from(self.rand.sample(self.distribution))
+        Mass::from(self.gen.next())
     }
 }
 
@@ -36,33 +54,34 @@ impl MassGen {
 mod tests {
     use physics::types::Mass;
     use util::gens::MassGen;
+    use util::gens::PositiveUniformGen;
 
     #[test]
     #[should_panic]
-    fn mass_gen_panics_on_non_positive_low() {
+    fn positive_uniform_gen_panics_on_non_positive_low() {
         // when
-        MassGen::new(-2.0, 2.0);
+        PositiveUniformGen::new(-2.0, 2.0);
     }
 
     #[test]
     #[should_panic]
-    fn mass_gen_panics_on_non_positive_high() {
+    fn positive_uniform_gen_panics_on_non_positive_high() {
         // when
-        MassGen::new(1.0, 0.0);
+        PositiveUniformGen::new(1.0, 0.0);
     }
 
     #[test]
     #[should_panic]
-    fn mass_gen_panics_on_invalid_range() {
+    fn positive_uniform_gen_panics_on_invalid_range() {
         // when
-        MassGen::new(2.0, 1.0);
+        PositiveUniformGen::new(2.0, 1.0);
     }
 
     #[test]
-    fn mass_gen_generates() {
+    fn positive_uniform_gen_generates() {
         // given
-        let mut sut = MassGen::new(1.0, 2.0);
-        let within_range = |m: Mass| m.value() >= 1.0 && m.value() <= 2.0;
+        let mut sut = PositiveUniformGen::new(1.0, 2.0);
+        let within_range = |n: f32| n >= 1.0 && n <= 2.0;
 
         // then
         assert!(within_range(sut.next()));
