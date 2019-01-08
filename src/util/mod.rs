a1612102c59f2bc98f3b26dbcee390a8f4199910
vancouver <vancouver2990@gmail.com>
2019-01-09 00:27:26 +0100

    Body writer
    bodies are written in a file

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -31,7 +31,7 @@ impl DataWriter {
 
     /// Creates a new file in the writers directory with each point written
     /// on a separate line.
-    pub fn write_point_file(&mut self, points: Vec<Point>) {
+    pub fn write(&mut self, points: Vec<Point>) {
         let path = format!("{}/frame-{}.txt", self.directory, self.counter);
         match self.write_points(points, path) {
             Err(e) => panic!("Error writing data. {}", e),
@@ -45,23 +45,32 @@ impl DataWriter {
         for point in points { write!(file, "{},{}\n", point.x, point.y)?; }
         Ok(())
     }
+}
 
-    /// Creates a new file in the writers directory with each body written on a separate line.
-    pub fn write_body_file(&mut self, bodies: Vec<Body>) {
-        let path = format!("{}/frame-{}.txt", self.directory, self.counter);
-        match self.write_bodies(bodies, path) {
-            Err(e) => panic!("Error writing bodies. {},", e),
-            Ok(_) => (),
+pub struct BodyWriter {
+    directory: String
+}
+
+impl BodyWriter {
+    /// creates a new directory in the current working path
+    pub fn new(directory: &str) -> BodyWriter {
+        if !Path::new(directory).exists() {
+            fs::create_dir(directory).expect("Could'nt create dir.");
         }
-        self.counter += 1;
+
+        BodyWriter {
+            directory: directory.to_owned()
+        }
     }
 
-    pub fn write_bodies(&self, bodies: Vec<Body>, path: String) -> std::io::Result<()> {
+    pub fn write_bodies(&self, bodies: Vec<Body> , path: String) -> std::io::Result<()> {
+        let path = format!("{}/{}.txt", self.directory, path);
         let mut file = fs::File::create(path)?;
         for body in bodies {
-            write!(file,"{}-{}-{}-{}-{}\n", body.mass, body.position.x, body.position.y,
-                                            body.velocity.dx, body.velocity.dy)?;
+            write!(file, "{}-{}-{}-{}-{}\n", body.mass, body.position.x, body.position.y,
+                   body.velocity.dx, body.velocity.dy)?;
         }
+
         Ok(())
     }
 }
@@ -77,47 +86,43 @@ mod tests {
     #[test]
     fn data_writer_writes() {
         // given
-        let mut writer_points = DataWriter::new("temp");
+        let mut writer = DataWriter::new("temp");
 
         // when
-        writer_points.write_point_file(vec![Point::new(3.4, 6.7)]);
-        writer_points.write_point_file(vec![Point::new(6.4, 6.785)]);
+        writer.write(vec![Point::new(3.4, 6.7)]);
+        writer.write(vec![Point::new(6.4, 6.785)]);
 
         // then
         let mut file = fs::File::open("temp/frame-0.txt").expect("Error opening file.");
         let mut contents = String::new();
-        file.read_to_string(&mut contents);
+        let _ = file.read_to_string(&mut contents);
         assert_eq!(contents, "3.4,6.7\n".to_owned());
 
         let mut file = fs::File::open("temp/frame-1.txt").expect("Error opening file.");
         let mut contents = String::new();
-        file.read_to_string(&mut contents);
+        let _ = file.read_to_string(&mut contents);
         assert_eq!(contents, "6.4,6.785\n".to_owned());
+
 
         // after
         fs::remove_dir_all("temp").expect("Error cleaning up test.");
     }
 
-    fn data_writer_writes_bodies(){
+    #[test]
+    fn body_writer() {
         // given
-        let mut writer_bodies = DataWriter::new("temp1");
+        let mut body_writer = BodyWriter::new("bodies_file");
 
         // when
-        writer_bodies.write_body_file(vec![Body::new(2.0,
-                                                     Point::new(1.0,2.0),
-                                                     Vector::zero())]);
-        writer_bodies.write_body_file(vec![Body::new(3.0,
-                                                            Point::new(4.0, 5.0),
-                                                            Vector::zero())]);
-        // then
-        let mut file = fs::File::open("temp1/frame-0.txt").expect("Error opening file.");
-        let mut contents = String::new();
-        file.read_to_string(&mut contents);
-        assert_eq!(contents, "2.0-1.0-2.0-0.0-0.0".to_owned());
+        body_writer.write_bodies(vec![Body::new(2.1, Point::new(1.1,2.2),
+                                                Vector::new(3.4, 4.5))], "bodies".to_owned());
+//        body_writer.write_bodies(vec![Body::new(3.0, Point::new(4.0,5.0),
+//                                                Vector::zero())], "bodies".to_owned());
 
-        let mut file = fs::File::open("temp1/frame-1.txt").expect("Error opening file.");
-        let mut contents = String::new();
-        file.read_to_string(&mut contents);
-        assert_eq!(contents, "3.0-4.0-5.0-0.0-0.0".to_owned());
+        // then
+        let mut file = fs::File::open("bodies_file/bodies.txt").expect("Error opening file.");
+        let mut lines = String::new();
+        file.read_to_string(&mut lines);
+        assert_eq!(lines, "2.1-1.1-2.2-3.4-4.5\n".to_owned());
     }
 }
