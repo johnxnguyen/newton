86d4b16732c9f63d6ae4009b37642beddc7b87f7
John Nguyen <johnxuannguyen@gmail.com>
2019-01-30 17:49:36 +0100

    Move configs files into directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-
-
 use newton::physics::types::Environment;
 use newton::util::distribution::Loader;
 
 fn main() {
     let mut loader = Loader::new();
-    let bodies = loader.load_from_path("Radial5000.yaml").unwrap();
+    let bodies = loader.load_from_path("configurations/Radial5000.yaml").unwrap();
     let mut env = Environment::new();
     env.bodies = bodies;
 
