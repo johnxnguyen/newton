973b5b3d1d262302649552f45ab06cbcb08bbb64
John Nguyen <johnxuannguyen@gmail.com>
2019-01-31 21:53:50 +0100

    Basic CLI implementation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use newton::physics::types::Environment;
 use newton::util::distribution::Loader;
 
-fn main() {
-    let mut loader = Loader::new();
-    let bodies = loader.load_from_path("configurations/Radial5000.yaml").unwrap();
-    let mut env = Environment::new();
-    env.bodies = bodies;
+use clap::App;
+use clap::load_yaml;
+use clap::value_t;
 
-    let upper = 5;
-    for x in 1..=upper {
-        println!("frame: {}/{}", x, upper);
+fn main() {
+    let yaml = load_yaml!("../cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let path = matches.value_of("INPUT").unwrap();
+    let frames = value_t!(matches, "FRAMES", u32).unwrap();
+
+    let mut env = Environment::new();
+    {
+        let mut loader = Loader::new();
+        env.bodies = loader.load_from_path(path).unwrap();
+    }
+
+    for x in 1..=frames {
+        println!("frame: {}/{}", x, frames);
         env.update();
     }
 }
